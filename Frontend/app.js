447a2e08a3d2e72dc8db93034a395a44d4956dc2
Brandon <brandonpoulin21@gmail.com>
2026-05-06 11:29:11 -0400

    Login functionality

--- Frontend/app.js
+++ Frontend/app.js
@@ -9,3 +9,32 @@ navLinks.forEach(link => {
         link.classList.add('active');
     }
 });
+
+// Invalid login handling
+document.getElementById("loginBtn").addEventListener("click", () => {
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("pwd").value;
+
+    const message = document.getElementById("loginMessage");
+
+    //--------------- Fake credentials for testing ----------------
+    if (username === "customer" && password === "1234") {
+        window.location.href = "customer.html";
+    }
+
+    else if (username === "banker" && password === "1234") {
+        window.location.href = "banker.html";
+    }
+
+    else if (username === "admin" && password === "1234") {
+        window.location.href = "admin.html";
+    }
+
+    else {
+        message.innerText = "Error: Invalid credentials";
+
+        document.getElementById("pwd").value = "";
+        document.getElementById("pwd").focus();
+    }
+});
