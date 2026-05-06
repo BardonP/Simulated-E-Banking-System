929b5db97895619c314032f2e8b0bd3897a95cfb
Brandon <brandonpoulin21@gmail.com>
2026-05-06 12:20:43 -0400

    MFA screen and functionality

--- Frontend/app.js
+++ Frontend/app.js
@@ -31,6 +31,7 @@ document.getElementById("loginBtn").addEventListener("click", () => {
         window.location.href = "admin.html";
     }
 
+    //Invalid credentials pop-up and reset password field
     else {
         message.innerText = "Error: Invalid credentials";
 
@@ -38,3 +39,71 @@ document.getElementById("loginBtn").addEventListener("click", () => {
         document.getElementById("pwd").focus();
     }
 });
+
+//------------MFA functionality --------------
+const sendCodeBtn = document.getElementById("sendCodeBtn");
+
+if (sendCodeBtn) {
+
+    sendCodeBtn.addEventListener("click", () => {
+
+        const username =
+            document.getElementById("mfaUsername").value;
+
+        const message =
+            document.getElementById("mfaMessage");
+
+        if (username === "") {
+
+            message.style.color = "red";
+            message.innerText =
+                "Error: Enter a username";
+
+            return;
+        }
+
+        // Hide username step
+        document.getElementById("usernameStep")
+            .style.display = "none";
+
+        // Show code step
+        document.getElementById("codeStep")
+            .style.display = "block";
+
+        message.style.color = "green";
+        message.innerText =
+            "Verification code sent successfully";
+    });
+}
+
+const verifyMfaBtn =
+    document.getElementById("verifyMfaBtn");
+
+if (verifyMfaBtn) {
+
+    verifyMfaBtn.addEventListener("click", () => {
+
+        const code =
+            document.getElementById("mfaCode").value;
+
+        const message =
+            document.getElementById("mfaMessage");
+
+        // Temporary fake code------------------------------------------------------DELETE LATER----------------------
+        if (code === "123456") {
+
+            window.location.href = "customer.html";
+
+        } else {
+
+            message.style.color = "red";
+
+            message.innerText =
+                "Error: Invalid verification code";
+
+            document.getElementById("mfaCode").value = "";
+
+            document.getElementById("mfaCode").focus();
+        }
+    });
+}
